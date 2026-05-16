8a0c4c91e14f199f5673f112012f2b05dcdafb6d
Honey Blip <honey8lip@users.noreply.github.com>
2026-05-17 03:54:05 +0800

    add optional flag to WizardStep, percent to WizardProgress, and getStepById to WizardInstance

--- src/formWizard.types.js
+++ src/formWizard.types.js
@@ -3,6 +3,7 @@
  * @property {string} id - Unique step identifier
  * @property {string} label - Human-readable step label
  * @property {import('./schemaParser.types').FieldSchema[]} fields - Fields for this step
+ * @property {boolean} [optional] - Whether the step can be skipped
  */
 
 /**
@@ -16,6 +17,7 @@
  * @typedef {Object} WizardProgress
  * @property {number} current - 1-based current step number
  * @property {number} total - Total number of steps
+ * @property {number} percent - Completion percentage (0-100)
  */
 
 /**
@@ -30,4 +32,5 @@
  * @property {function(): boolean} prev
  * @property {function(function=): boolean} complete
  * @property {function(): void} reset
+ * @property {function(string): WizardStep|undefined} getStepById - Find a step by its id
  */
